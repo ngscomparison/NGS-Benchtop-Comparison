5f121a527f14e94694a439e1df4ae2fc507d0b3d
Sebastian Juenemann <jueneman@bruce.CeBiTec.Uni-Bielefeld.DE>
2013-03-28 16:46:04 +0100

    initial set up

--- assemblyvariants/VennChart/variants_assembly.R
+++ assemblyvariants/VennChart/variants_assembly.R
@@ -4,7 +4,7 @@ library("VennDiagram")
 # like the Consensus_differences.R, but takes not read the gerneic mot files but a plain list (eiher subst or indel) 
 # of manually extracted variance position (one line per technology with one column per line only lisitng the variance names-positions as one entry (seperated by ",")
 
-data=read.table("SeqSphereSNPs.csv", header=T,sep='\t')
+data=read.table("corrected_variances/SNPs_all.csv", header=T,sep='\t')
 PGM_snp=data[1,2]
 PGM_snp=unlist(strsplit(gsub("\\s","", as.character(PGM_snp)), split=","))
 MiSeq_snp=data[2,2]
@@ -19,7 +19,7 @@ venn.diagram(
         MiSeq=MiSeq_snp,
         GSJ=GSJ_snp
         ),
-    filename = "SeqShpereVaraints_snp.tiff",
+    filename = "Varaints_SNPs.tiff",
     col = "transparent",
     fill = c("red", "blue", "green"),
     alpha = 0.5,
@@ -34,7 +34,7 @@ venn.diagram(
     cat.pos = 0
     );
 
-data=read.table("SeqSphereIndels.csv", header=T,sep='\t')
+data=read.table("corrected_variances/InDels_all.csv", header=T,sep='\t')
 PGM_ind=data[1,2]
 PGM_ind=unlist(strsplit(gsub("\\s","", as.character(PGM_ind)), split=","))
 MiSeq_ind=data[2,2]
@@ -48,7 +48,7 @@ venn.diagram(
         MiSeq=MiSeq_ind,
         GSJ=GSJ_ind
         ),
-    filename = "SeqSphereVariants_indel.tiff",
+    filename = "Variants_InDels.tiff",
     col = "transparent",
     fill = c("green", "red", "blue"),
     alpha = 0.5,
